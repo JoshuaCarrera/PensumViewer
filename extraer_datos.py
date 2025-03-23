1f745d57cdabc79d73376563690bc2cd9e894f86
JoshuaCarrera <cubyt3@gmail.com>
2025-03-23 02:00:03 -0400

    Añadir código para visualizar la totalidad del pdf

--- extraer_datos.py
+++ extraer_datos.py
@@ -1,16 +1,18 @@
 import camelot
 import pandas as pd
 
-# Especificar las páginas que deseas leer (por ejemplo, página 1 y 2)
-tables = camelot.read_pdf('doc.pdf', pages='1,2')
+tables = camelot.read_pdf('doc.pdf', pages='all')
 print(f"Número de tablas detectadas: {len(tables)}")
 
 # Crear una lista para almacenar los DataFrames de cada tabla
 dataframes = []
 
 # Iterar sobre cada tabla y agregar su DataFrame a la lista
-for table in tables:
-    dataframes.append(table.df)
+for i, table in enumerate(tables):
+    df = table.df
+    if i > 0:  # Si no es la primera tabla, elimina la primera fila (encabezados)
+        df = df[1:]
+    dataframes.append(df)
 
 # Concatenar todos los DataFrames en uno solo
 combined_df = pd.concat(dataframes, ignore_index=True)
@@ -20,6 +22,9 @@ nuevos_nombres = combined_df.iloc[0]  # Obtener la primera fila
 combined_df = combined_df[1:]  # Eliminar la primera fila del DataFrame
 combined_df.columns = nuevos_nombres  # Asignar los nuevos nombres de columnas
 combined_df = combined_df.reset_index(drop=True)
+
+# Eliminar filas duplicadas
+combined_df = combined_df.drop_duplicates(subset=['Código'])
 
 # Guardar el DataFrame combinado en un único archivo CSV
 combined_df.to_csv('doc.csv', index=False)
