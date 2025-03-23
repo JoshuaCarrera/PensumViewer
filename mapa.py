9397c246504f78d0ab9cdc1c419bfe2bfae5f294
JoshuaCarrera <cubyt3@gmail.com>
2025-03-23 03:21:35 -0400

    Agregar el '---' para tener un centro más claro

--- mapa.py
+++ mapa.py
@@ -56,29 +56,28 @@ for _, row in relaciones.iterrows():
     codigo = row['Código']
     prelacion = row['Prelación']
     
-    if prelacion != '---':
-        # Separar las prelaciones en grupos AND y OR
-        if type(prelacion) == str:
-            for grupo in prelacion.split('|'):  # Primero separar por OR
-                # Determinar el tipo de relación (AND u OR)
-                if '&' in grupo:
-                    tipo_relacion = 'AND'
-                    prereqs = grupo.split('&')
-                else:
-                    tipo_relacion = 'OR'
-                    prereqs = [grupo]  # Si no hay &, es un solo requisito (OR)
+    # Separar las prelaciones en grupos AND y OR
+    if type(prelacion) == str:
+        for grupo in prelacion.split('|'):  # Primero separar por OR
+            # Determinar el tipo de relación (AND u OR)
+            if '&' in grupo:
+                tipo_relacion = 'AND'
+                prereqs = grupo.split('&')
+            else:
+                tipo_relacion = 'OR'
+                prereqs = [grupo]  # Si no hay &, es un solo requisito (OR)
+            
+            # Agregar nodos y edges según el tipo de relación
+            for prereq in prereqs:
+                # Si el prereq no es un código de curso, se agrega como un nodo adicional
+                if prereq not in relaciones['Código'].values:
+                    net.add_node(prereq, prereq, title=prereq, color="#FFA500", shape="dot", value=2)  # Color naranja para requisitos generales
                 
-                # Agregar nodos y edges según el tipo de relación
-                for prereq in prereqs:
-                    # Si el prereq no es un código de curso, se agrega como un nodo adicional
-                    if prereq not in relaciones['Código'].values:
-                        net.add_node(prereq, prereq, title=prereq, color="#FFA500", shape="dot", value=2)  # Color naranja para requisitos generales
-                    
-                    # Agregar el edge entre el prereq y el código
-                    if tipo_relacion == 'AND':
-                        net.add_edge(prereq, codigo, title="AND")  # Línea roja para AND
-                    else:
-                        net.add_edge(prereq, codigo, title="OR", dashes=True)  # Línea verde punteada para OR
+                # Agregar el edge entre el prereq y el código
+                if tipo_relacion == 'AND':
+                    net.add_edge(prereq, codigo, title="AND")  # Línea roja para AND
+                else:
+                    net.add_edge(prereq, codigo, title="OR", dashes=True)  # Línea verde punteada para OR
 
 # Generar el grafo
 net.save_graph("index.html")
